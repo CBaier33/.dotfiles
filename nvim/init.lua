2d2d30d51983b71e64166bc4ed9d570779b1bdd4
Christopher Baier <baier.tech@outlook.com>
2024-08-17 01:04:51 -0400

    updates

--- nvim/init.lua
+++
@@ -1,9 +0,0 @@
-require("remap")
-require("set")
-
-require("plugins.plugins")
-require("plugins.treesitter")
-require("plugins.harpoon")
-require("plugins.lsp")
-
-require('monokai').setup { palette = require('monokai').ristretto };
